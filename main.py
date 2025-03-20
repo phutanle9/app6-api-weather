d94c54378f9bf28d15c4bd771478f8e70f47e9aa
phutanle9 <135482723+phutanle9@users.noreply.github.com>
2025-03-21 00:54:59 +0700

    add gitignore

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
+import pandas as pd
 
-app= Flask(__name__)
+app = Flask(__name__)
 
+stations =pd.read_csv("data_small/stations.txt", skiprows=17)
+# stations = stations[['STAID',"STANAME"]]
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",data=stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
-def about(station,date):
-    temperature=23
-    return {"station" : station,
-            "date": date,
-            "temperature":temperature}
+def about(station, date):
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
 
-if __name__ == " main ":
+    # Clean up column names by stripping spaces
+    df.columns = df.columns.str.strip()
+
+    # Retrieve temperature for the given station and date
+    temperature = df.loc[df['    DATE'] == date, '   TG'].squeeze() / 10
+
+    return {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
+
+if __name__ == "__main__":
     app.run(debug=True)
